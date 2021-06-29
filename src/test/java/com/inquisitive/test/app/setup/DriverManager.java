2435f4b543e35f6c2da7ca9c2594eee9dc58a879
admin <celinjustus@gmail.com>
2021-06-29 19:15:38 +1000

    WebDriver manager updated

--- src/test/java/com/inquisitive/test/app/setup/DriverManager.java
+++ src/test/java/com/inquisitive/test/app/setup/DriverManager.java
@@ -1,6 +1,7 @@
 package com.inquisitive.test.app.setup;
 
 import com.inquisitive.test.utils.App;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -10,16 +11,16 @@ public class DriverManager {
     public static WebDriver getDriver(String browser) {
         WebDriver driver;
         if (browser.equalsIgnoreCase("chrome")) {
-            //WebDriverManager.chromedriver().setup();
-            System.setProperty("webdriver.chrome.driver", App.getProperty("chrome.driver.location"));
+            WebDriverManager.chromedriver().setup();
+            //System.setProperty("webdriver.chrome.driver", App.getProperty("chrome.driver.location"));
             driver = new ChromeDriver();
         } else if (browser.equalsIgnoreCase("edge")) {
-            //WebDriverManager.edgedriver().setup();
-            System.setProperty("webdriver.edge.driver", App.getProperty("edge.driver.location"));
+            WebDriverManager.edgedriver().setup();
+           // System.setProperty("webdriver.edge.driver", App.getProperty("edge.driver.location"));
             driver = new EdgeDriver();
         } else {
-            //WebDriverManager.iedriver().setup();
-            System.setProperty("webdriver.ie.driver", App.getProperty("ie.driver.location"));
+            WebDriverManager.iedriver().setup();
+           // System.setProperty("webdriver.ie.driver", App.getProperty("ie.driver.location"));
             driver = new InternetExplorerDriver();
         }
         return driver;
